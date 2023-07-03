67aea2f8ca3dbdf09623f5104d18a10a37b9b853
Zencodetech <137770092+Zencodetech@users.noreply.github.com>
2023-07-03 19:47:55 +0700

    Chapter 1

---
+++ Chapter1/exercise1c.java
@@ -0,0 +1,12 @@
+public class exercise1c {
+    public static void main(String[]args){
+        double w = 4.5, h = 7.9; 
+        double area = w * h;
+        double peri = 2 * (h + w);
+        System.out.println("Find the area of rectangle");
+        System.out.println("Area of rectangle is : "+area);
+        System.out.println("Find perimeter of rectangle");
+        System.out.println("Perimeter of rectangle is : "+peri);
+    }
+
+}
