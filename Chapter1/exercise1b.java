f969841e8d6459b3392e8f4f9b596c22f7aaee1c
Zencodetech <137770092+Zencodetech@users.noreply.github.com>
2023-07-03 19:44:05 +0700

    Chapter 1

---
+++ Chapter1/exercise1b.java
@@ -0,0 +1,7 @@
+public class exercise1b {
+    public static void main(String[]args){
+        System.out.println("((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5)");
+        double a = (double) (((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+        System.out.println(a);
+    }
+}
